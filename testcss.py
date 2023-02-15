34c0055caf0c3512f9e918d604021719aa943505
jiaaoc <chenjiaao@yahoo.com>
2023-02-15 15:50:14 -0500

    update codes

--- testcss.py
+++ testcss.py
@@ -8,7 +8,7 @@ import re
 import random
 import argparse
 #from revChatGPT.ChatGPT import Chatbot
-from revChatGPT.Official import Chatbot
+from revChatGPT.V2 import Chatbot
 
 
 def get_input(prompt):
@@ -33,8 +33,9 @@ def configure(args):
         rationalepath = args.rantionalepath
         answerpath = args.answerpath
 
-        getrationales(inputpath, rationalepath, args)
-        getanswers(rationalepath, answerpath, args)
+        #getrationales(inputpath, rationalepath, args)
+        #getanswers(rationalepath, answerpath, args)
+        getanswers(inputpath, answerpath, args)
         calculateres(answerpath,args)
     except KeyboardInterrupt:
         print("\nGoodbye!")
@@ -158,7 +159,9 @@ def getanswers(inputpath, outputpath, args):
     start = time.time()
 
     with open(inputpath, "r", encoding="utf-8") as f:
-        count = len(f.readlines())
+        raw_data = json.load(f)
+    count = len(raw_data['Label'])
+    #   count = len(f.readlines())
     print("number of data: ", count)
     allflag = [0 for i in range(count)]  ####the number of all samples
 
@@ -184,23 +187,26 @@ def getanswers(inputpath, outputpath, args):
         testsamples = []
         goldlabel = []
         touseindex = []
+        
         with open(inputpath, "r", encoding="utf-8") as f:
+            raw_data = json.load(f)
+            conversations = raw_data['Conversation']
             i = 0
-            for oneline in f:
+            for (u,v) in raw_data['Conversation'].items():
                 if allflag[i] == 1:
                     i += 1
                     continue
-                onedata = oneline.strip().split('\t')
-                wrap_que = onedata[2].replace('&&&&&&','\n')
-                testsamples.append(wrap_que)
-                goldlabel.append(onedata[1])
+                #onedata = oneline.strip().split('\t')
+                #wrap_que = onedata[2].replace('&&&&&&','\n')
+                testsamples.append(v)
+                goldlabel.append(raw_data['Label'][u])
                 touseindex.append(i)
                 i += 1
 
         inputprompts = []
         for i in range(len(testsamples)):
             oneres = testsamples[i]
-            inputprompts.append(oneres)
+            inputprompts.append(oneres + args.direct_answer_trigger_for_zeroshot_cot)
             #print(oneres)
         #exit -1
         fw = open(outputpath, "a+", encoding="utf-8")
@@ -243,127 +249,38 @@ def calculateres(path, args):
             continue
         index = int(content[0])
         allnum += 1
-        if args.dataset in ("gsm8k", "addsub", "multiarith", "svamp", "singleeq"):
-            gold = float(content[1])
-        else:
-            gold = content[1]
-        pred = content[2]
-        pred = pred.replace("&&&&&&", "")
-
-        if args.dataset in ("aqua", "commonsensqa"):
-            pred = re.findall(r'A|B|C|D|E', pred)
-        elif args.dataset == "bigbench_date":
-            pred = re.findall(r'A|B|C|D|E|F', pred)
-        elif args.dataset in ("object_tracking"):
-            pred = re.findall(r'A|B|C', pred)
-        elif args.dataset in ("gsm8k", "addsub", "multiarith", "svamp", "singleeq"):
-            pred = pred.replace(",", "")
-            pred = [s for s in re.findall(r'-?\d+\.?\d*', pred)]
-        elif args.dataset in ("strategyqa", "coin_flip"):
-            pred = pred.lower()
-            pred = re.sub("\"|\'|\n|\.|\s|\:|\,", " ", pred)
-            pred = pred.split(" ")
-            pred = [i for i in pred if i in ("yes", "no")]
-        elif args.dataset == "last_letters":
-            pred = re.sub("\"|\'|\n|\.|\s", "", pred)
-            pred = [pred]
-        else:
-            raise ValueError("dataset is not properly defined ...")
-
-        if len(pred) == 0:
-            print("no answer!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            pred = ""
-        else:
-            pred = pred[0]
-
-        if pred != "":
-            if pred[-1] == ".":
-                pred = pred[:-1]
-
+        #if args.dataset in ("gsm8k", "addsub", "multiarith", "svamp", "singleeq"):
+        #    gold = float(content[1])
+        #else:
+            #gold = content[1]
+        gold = content[1] == 'true'
+        
+        #pred = 'True' in content[2]
+        pred = 'true' in pred.replace("&&&&&&", "").lower()
+        
         print(gold, pred, content[2])
-        if args.dataset in ("gsm8k", "addsub", "multiarith", "svamp", "singleeq"):
-            if pred != "":
-                if float(pred) == gold:
-                    accnum += 1
-        else:
-            if pred == gold:
-                accnum += 1
+        if pred == gold:
+            accnum += 1
 
     print(allnum, accnum, float(accnum) / float(allnum))
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="chatgpt-zero-shot-coT")
+    parser = argparse.ArgumentParser(description="chatgpt-zero-shot-css")
 
     parser.add_argument(
-        "--dataset", type=str, default="gsm8k",
-        choices=["aqua", "gsm8k", "commonsensqa", "addsub", "multiarith", "strategyqa", "svamp", "singleeq",
-                 "coin_flip", "last_letters", "bigbench_date", "object_tracking"], help="dataset used for experiment"
+        "--dataset", type=str, default="conv_go_awry",
+        choices=["conv_go_awry"], help="dataset used for experiment"
     )
     args = parser.parse_args()
-    if args.dataset == "aqua":
-        args.inputpath = "data/reasoning/aqua_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/aqua_rationale_cot"
-        args.answerpath = "data/reasoning/aqua_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, among A through E, the answer is"
-    elif args.dataset == "gsm8k":
-        args.inputpath = "data/reasoning/gsm8k_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/gsm8k_rationale_cot"
-        args.answerpath = "data/reasoning/gsm8k_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (arabic numerals) is"
-    elif args.dataset == "commonsensqa":
-        args.inputpath = "data/reasoning/commonsensqa_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/commonsensqa_rationale_cot"
-        args.answerpath = "data/reasoning/commonsensqa_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, among A through E, the answer is"
-    elif args.dataset == "addsub":
-        args.inputpath = "data/reasoning/addsub_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/addsub_rationale_cot"
-        args.answerpath = "data/reasoning/addsub_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (arabic numerals) is"
-    elif args.dataset == "multiarith":
-        args.inputpath = "data/reasoning/multiarith_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/multiarith_rationale_cot"
-        args.answerpath = "data/reasoning/multiarith_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (arabic numerals) is"
-    elif args.dataset == "strategyqa":
-        args.inputpath = "data/reasoning/strategyqa_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/strategyqa_rationale_cot"
-        args.answerpath = "data/reasoning/strategyqa_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (Yes or No) is"
-    elif args.dataset == "svamp":
-        args.inputpath = "data/reasoning/svamp_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/svamp_rationale_cot"
-        args.answerpath = "data/reasoning/svamp_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (arabic numerals) is"
-    elif args.dataset == "singleeq":
-        args.inputpath = "data/reasoning/singleeq_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/singleeq_rationale_cot"
-        args.answerpath = "data/reasoning/singleeq_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (arabic numerals) is"
-    elif args.dataset == "bigbench_date":
-        args.inputpath = "data/reasoning/date_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/date_rationale_cot"
-        args.answerpath = "data/reasoning/date_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, among A through F, the answer is"
-    elif args.dataset == "object_tracking":
-        args.inputpath = "data/reasoning/object_tracking_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/object_tracking_rationale_cot"
-        args.answerpath = "data/reasoning/object_tracking_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, among A through C, the answer is"
-    elif args.dataset == "coin_flip":
-        args.inputpath = "data/reasoning/coin_flip_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/coin_flip_rationale_cot"
-        args.answerpath = "data/reasoning/coin_flip_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer (Yes or No) is"
-    elif args.dataset == "last_letters":
-        args.inputpath = "data/reasoning/last_letters_zero_shot_cot"
-        args.rantionalepath = "data/reasoning/last_letters_rationale_cot"
-        args.answerpath = "data/reasoning/last_letters_answer_cot"
-        args.direct_answer_trigger_for_zeroshot_cot = "&&&&&&Therefore, the answer is"
+    if args.dataset == "conv_go_awry":
+        args.inputpath = "css_data/aqua_zero_shot_cot"
+        args.rantionalepath = "css_data/aqua_rationale_cot"
+        args.answerpath = "css_data/aqua_answer_cot"
+        
+        args.direct_answer_trigger_for_zeroshot_cot = "Predict whether the given conversation has a personal attack (True or False)."
     else:
         raise ValueError("dataset is not properly defined ...")
-    args.api_key = "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"
-    # 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
+    args.api_key = "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"
     return args
 
 def main():
