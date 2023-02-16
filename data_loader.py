671bf975c092d44ada46d0f657192a53877c6f10
Helw150 <wbh230@nyu.edu>
2023-02-16 16:10:53 -0500

    Correct Indentation of Input

--- data_loader.py
+++ data_loader.py
@@ -2,34 +2,7 @@ import argparse
 import os
 import pandas as pd
 from convokit import Corpus, download
-
-convokit_ds = {
-    "discourse": "reddit-coarse-discourse-corpus",
-    "persuasion": "winning-args-corpus",
-    "toxicity": "conversations-gone-awry-corpus",
-    "politeness": "wikipedia-politeness-corpus",
-    "stance": "supreme-corpus",
-    "power": "wiki-corpus",
-}
-
-# (Where is the Label Stored, What is the Label Applied to, how much context)
-convokit_labels = {
-    "power": ("speaker", "meta.is-admin", "all"),
-    "stance": ("speaker", "meta.side", "all"),
-    "politeness": ("utterance", "meta.Binary", "all"),
-    "toxicity": ("conversation", "meta.conversation_has_personal_attack", "first_two"),
-    "persuasion": ("utterance", "meta.success", "first_two"),
-    "discourse": ("utterance", "meta.majority_type", "all"),
-}
-
-convokit_prompts = {
-    "power": "Is {$speaker} an administrator (Yes or No)?",
-    "stance": "Does {$speaker} support {$title} (Yes or No)?",  # This Dataset is strange currently, too long of context
-    "politeness": "Was this statement polite (Yes or No)? ",
-    "toxicity": "Predict whether the given conversation has a personal attack (True or False).",
-    "persuasion": "Does this reply convince the original poster (Yes Or No)?",
-    "discourse": "Which of the following best characterizes the previous statement: question, answer, announcement, agreement, appreciation, disagreement, elaboration, or humor? ",
-}
+from mappings import *
 
 
 def deep_get(recursive_key, recursive_dict):
@@ -52,7 +25,6 @@ def convokit_process(dataset, save_dir):
     label_utterance_ids = []
     speaker_utterance_maps = []
     label_type, label_field, context_length = convokit_labels[dataset]
-    print(label_type)
     for _, convo in df.groupby("conversation_id").__iter__():
         if context_length == "all":
             convo = convo.sort_values(by="timestamp")
@@ -62,7 +34,6 @@ def convokit_process(dataset, save_dir):
                 speaker if type(speaker) == type("str") else speaker.id
                 for speaker in speakers
             ]
-            print(speakers)
             utterance_ids = convo.index.values.tolist()
             context = [
                 "{}: {}".format(speaker, context)
@@ -114,7 +85,7 @@ def convokit_process(dataset, save_dir):
                 label = deep_get(label_field, utterance)
                 labels.append(label)
                 prompts.append(convokit_prompts[dataset].replace("{$speaker}", speaker))
-                contexts = speaker_contexts
+        contexts = speaker_contexts
     elif label_type == "conversation":
         for conversation_id in conversation_ids:
             conversation = corpus.get_conversation(conversation_id).to_dict()
---
+++ mappings.py
@@ -0,0 +1,27 @@
+convokit_ds = {
+    "discourse": "reddit-coarse-discourse-corpus",
+    "persuasion": "winning-args-corpus",
+    "toxicity": "conversations-gone-awry-corpus",
+    "politeness": "wikipedia-politeness-corpus",
+    "stance": "supreme-corpus",
+    "power": "wiki-corpus",
+}
+
+# (Where is the Label Stored, What is the Label Applied to, how much context)
+convokit_labels = {
+    "power": ("speaker", "meta.is-admin", "all"),
+    "stance": ("speaker", "meta.side", "all"),
+    "politeness": ("utterance", "meta.Binary", "all"),
+    "toxicity": ("conversation", "meta.conversation_has_personal_attack", "first_two"),
+    "persuasion": ("utterance", "meta.success", "first_two"),
+    "discourse": ("utterance", "meta.majority_type", "all"),
+}
+
+convokit_prompts = {
+    "power": "Is {$speaker} an administrator (Yes or No)?",
+    "stance": "Does {$speaker} support {$title} (Yes or No)?",  # This Dataset is strange currently, too long of context
+    "politeness": "Was this statement polite (Yes or No)? ",
+    "toxicity": "Predict whether the given conversation has a personal attack (True or False).",
+    "persuasion": "Does this reply convince the original poster (Yes Or No)?",
+    "discourse": "Which of the following best characterizes the previous statement: question, answer, announcement, agreement, appreciation, disagreement, elaboration, or humor? ",
+}
