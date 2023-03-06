4fdcd20c3cc34d731281adc5bf5974fc78360cce
Caleb Ziems <cjziems@gmail.com>
2023-03-06 09:13:38 -0500

    sbic prompt update

--- mappings.py
+++ mappings.py
@@ -212,7 +212,7 @@ B: Different
 Constraint: Answer with only the option above that is most accurate and nothing else.""",
     "sbic": """
     
-If the above sentence is offensive to an identity group, what is the implied aspect of that group that this sentence communicates? Please use a phrase like "GROUP are ______" (e.g., "Korean folks have weird names")
+Explain the stereotype that this sentence communicates? Please use a single sentence like "[GROUP] are ______"
 """,
 
 "talklife": """
