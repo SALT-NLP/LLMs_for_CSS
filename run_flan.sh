269dd32b816adb9f3d0e22189b9719af50a73d60
Omar Shaikh <oshaikh13@gmail.com>
2023-03-08 16:40:35 -0800

    update

--- run_flan.sh
+++ run_flan.sh
@@ -1,3 +1,5 @@
+
+export TRANSFORMERS_CACHE=/nlp/scr/oshaikh/flan-cache
 declare -a arr=("conv_go_awry" "wiki_corpus" "implicit_hate" "discourse" "reddit_humor" "flute-explanation" "flute-classification" "supreme_corpus" "wiki_politeness" "media_ideology" "hippocorpus" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "sbic" "talklife" "raop" "emotion" "mrf-explanation" "mrf-classification" "tropes")
 
 for i in "${arr[@]}"
