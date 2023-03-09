8cfa6fb53d4789377d156f79164e0bce1182ebda
Omar Shaikh <oshaikh13@gmail.com>
2023-03-08 16:59:24 -0800

    fix bash script AGAIN

--- run_flan.sh
+++ run_flan.sh
@@ -1,5 +1,6 @@
 export TRANSFORMERS_CACHE=/nlp/scr/oshaikh/flan-cache
-declare -a arr=("hate" "reframe" "humor" "flute-explanation" "flute-classification" "media_ideology" "hippocorpus" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "talklife" "sbic" "raop" "emotion" "mrf-explanation" "mrf-classification" "tropes")
+declare -a arr=("conv_go_awry" "wiki_corpus" "implicit_hate" "discourse" "reddit_humor" "flute-explanation" "flute-classification" "supreme_corpus" "wiki_politeness" "media_ideology" "hippocorpus" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "sbic" "talklife" "raop" "emotion" "mrf-explanation" "mrf-classification" "tropes")
+
 
 for i in "${arr[@]}"
 do
