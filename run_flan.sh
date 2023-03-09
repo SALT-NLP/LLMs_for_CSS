041e52df6560b2798c29b850f24b1e2218176596
Omar Shaikh <oshaikh13@gmail.com>
2023-03-08 16:28:51 -0800

    add scrape script

---
+++ run_flan.sh
@@ -0,0 +1,7 @@
+declare -a arr=("conv_go_awry" "wiki_corpus" "implicit_hate" "discourse" "reddit_humor" "flute-explanation" "flute-classification" "supreme_corpus" "wiki_politeness" "media_ideology" "hippocorpus" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "sbic" "talklife" "raop" "emotion" "mrf-explanation" "mrf-classification" "tropes")
+
+for i in "${arr[@]}"
+do
+    python test_official_chat_css.py --dataset "$i" -m google/flan-t5-xxl -g 2
+done
+
