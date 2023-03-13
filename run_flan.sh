b194c17a31bd0ef4aaae069ba8f84b3a648f84a7
Omar Shaikh <oshaikh13@gmail.com>
2023-03-13 16:21:45 -0700

    fix script

--- run_flan.sh
+++ run_flan.sh
@@ -1,7 +1,7 @@
 export TRANSFORMERS_CACHE=/nlp/scr/oshaikh/flan-cache
 declare -a arr=("conv_go_awry" "power" "hate" "discourse" "humor" "flute-explanation" "flute-classification" "supreme_corpus" "politeness" "media_ideology" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "talklife" "raop" "emotion") 
 
-declare -a arr=("hippocorpus" "mrf-explanation" "mrf-classification" "tropes" "sbic") 
+# declare -a arr=("hippocorpus" "mrf-explanation" "mrf-classification" "tropes" "sbic") 
 
 for i in "${arr[@]}"
 do
