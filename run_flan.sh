cccf118cbb39179be853a03f3e895b38780b8be3
Omar Shaikh <oshaikh13@gmail.com>
2023-03-13 16:21:09 -0700

    update run-flan

--- run_flan.sh
+++ run_flan.sh
@@ -1,9 +1,19 @@
 export TRANSFORMERS_CACHE=/nlp/scr/oshaikh/flan-cache
-declare -a arr=("conv_go_awry" "power" "hate" "discourse" "humor" "flute-explanation" "flute-classification" "supreme_corpus" "politeness" "media_ideology" "hippocorpus" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "sbic" "talklife" "raop" "emotion" "mrf-explanation" "mrf-classification" "tropes") 
+declare -a arr=("conv_go_awry" "power" "hate" "discourse" "humor" "flute-explanation" "flute-classification" "supreme_corpus" "politeness" "media_ideology" "indian_english_dialect" "ibc" "semeval_stance" "tempowic" "talklife" "raop" "emotion") 
+
+declare -a arr=("hippocorpus" "mrf-explanation" "mrf-classification" "tropes" "sbic") 
 
 for i in "${arr[@]}"
 do
     echo "USING: $i"
     python test_official_chat_css.py --dataset "$i" -m google/flan-t5-xxl -g 2
 done
+
+
+# for i in "${arr[@]}"
+# do
+#     echo "USING: $i"
+#     python test_official_chat_css.py --dataset "$i"
+# done
+
 
