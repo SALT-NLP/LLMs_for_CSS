0ad2e5b3440c25ea419d76c4f298639a1f2017e7
jiaaoc <jiaaochen@gatech.edu>
2023-03-14 22:25:55 +0000

    update mrf-explanation

---
+++ run_generation.sh
@@ -0,0 +1,8 @@
+python test_official_chat_css.py --dataset sbic -m text-ada-001
+python test_official_chat_css.py --dataset sbic -m text-babbage-001
+python test_official_chat_css.py --dataset sbic -m text-curie-001
+python test_official_chat_css.py --dataset sbic -m text-davinci-001
+python test_official_chat_css.py --dataset sbic -m text-davinci-002
+python test_official_chat_css.py --dataset sbic -m text-davinci-003
+
+
