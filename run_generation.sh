dd33c4bd7a78102ced292270f0dc64949e60dc62
jiaaoc <jiaaochen@gatech.edu>
2023-03-14 22:56:40 +0000

    update gpt3 generation results

--- run_generation.sh
+++ run_generation.sh
@@ -6,3 +6,9 @@ python test_official_chat_css.py --dataset sbic -m text-davinci-002
 python test_official_chat_css.py --dataset sbic -m text-davinci-003
 
 
+python test_official_chat_css.py --dataset mrf-explanation -m text-ada-001
+python test_official_chat_css.py --dataset mrf-explanation -m text-babbage-001
+python test_official_chat_css.py --dataset mrf-explanation -m text-curie-001
+python test_official_chat_css.py --dataset mrf-explanation -m text-davinci-001
+python test_official_chat_css.py --dataset mrf-explanation -m text-davinci-002
+python test_official_chat_css.py --dataset mrf-explanation -m text-davinci-003
